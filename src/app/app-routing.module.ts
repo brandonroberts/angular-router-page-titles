83e0a81449bdec51351ed755ee885f9af45c465e
Brandon Roberts <robertsbt@gmail.com>
2022-01-31 20:21:23 -0600

    feat: update example to use custom title strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import { AboutComponent } from './about.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    component: HomeComponent,
+    title: 'Home'
   },
   {
     path: 'about',
-    loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutModule),
-  },
+    component: AboutComponent,
+    title: 'About Me'
+  }
 ];
+
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My App - ${title}`);
+    }
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }
+  ]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -29,29 +26,4 @@ import { filter, map } from 'rxjs';
 })
 export class AppComponent {
   title = 'angular-router-page-titles';
-
-  constructor(private router: Router, private titleService: Title) {}
-
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        map(() => {
-          let route: ActivatedRoute = this.router.routerState.root;
-          let routeTitle = '';
-          while (route!.firstChild) {
-            route = route.firstChild;
-          }
-          if (route.snapshot.data['title']) {
-            routeTitle = route!.snapshot.data['title'];
-          }
-          return routeTitle;
-        })
-      )
-      .subscribe((title: string) => {
-        if (title) {
-          this.titleService.setTitle(`My App - ${title}`);
-        }
-      });
-  }  
 }
